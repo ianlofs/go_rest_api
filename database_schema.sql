ecc2f51e7bd94525b1f558332eb31ea1c4f153de
ianlofs <i.lof33@gmail.com>
2016-05-16 15:57:31 -0700

    added procfile and update db schema file so it works for postgres

--- database_schema.sql
+++ database_schema.sql
@@ -1,37 +1,37 @@
-CREATE DATABASE IF NOT EXISTS `user_health_information`;
-\connect `user_health_information`
 
 --
 -- Table structure for users --
 --
 
-CREATE TABLE IF NOT EXISTS `user`(
-  `id` INT PRIMARY KEY,
-  `name` VARCHAR(256) NOT NULL,
-  `email` VARCHAR(256) NOT NULL
-  `username` VARCHAR(256) NOT NULL UNIQUE,
-  UNIQUE,
+CREATE TABLE users(
+  id INT,
+  name VARCHAR(256) NOT NULL,
+  email VARCHAR(256) NOT NULL,
+  username VARCHAR(256) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  PRIMARY KEY(id),
+  UNIQUE(email),
+  UNIQUE(username)
 );
-
-CREATE INDEX IF NOT EXISTS `name_idx` ON user(`name`);
+CREATE INDEX IF NOT EXISTS name_idx ON users(name);
 
 
 --
 -- Table structure for table health_stats --
 --
 
-CREATE TABLE IF NOT EXISTS `health_stats` (
-  `id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `step_count` INT NOT NULL,
-  `flights_climbed` INT NOT NULL,
-  `distance` FLOAT NOT NULL,
-  `time` DATETIME NOT NULL,
-  PRIMARY KEY(`id`),
-  FOREIGN KEY(`user_id`) REFERENCES user(`id`),
+CREATE TABLE health_stats(
+  id INT NOT NULL,
+  user_id INT NOT NULL,
+  step_count INT NOT NULL,
+  flights_climbed INT NOT NULL,
+  distance FLOAT NOT NULL,
+  time timestamp NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE INDEX IF NOT EXISTS `step_count_idx` ON health_stats(`step_count`);
-CREATE INDEX IF NOT EXISTS `flights_climbed_idx` ON health_stats(`flights_climbed`);
-CREATE INDEX IF NOT EXISTS `distance_idx` ON health_stats(`distance`);
-CREATE INDEX IF NOT EXISTS `time_idx` ON health_stats(`time`);
+CREATE INDEX IF NOT EXISTS step_count_idx ON health_stats(step_count);
+CREATE INDEX IF NOT EXISTS flights_climbed_idx ON health_stats(flights_climbed);
+CREATE INDEX IF NOT EXISTS distance_idx ON health_stats(distance);
+CREATE INDEX IF NOT EXISTS time_idx ON health_stats(time);
