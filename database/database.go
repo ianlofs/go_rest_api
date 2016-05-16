f23b67483fd71cf5faa2361f0d9b2caf8ce31d74
ianlofs <i.lof33@gmail.com>
2016-05-16 15:43:57 -0700

    added port and host for db conn

--- database/database.go
+++ database/database.go
@@ -13,7 +13,8 @@ type DB struct {
 }
 
 func New() (DB, error) {
-	conn, err := sql.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s sslmode=%s", os.Getenv("DB_USER"), os.Getenv("DB_TABLE"), os.Getenv("DB_PSSWRD"), "disable"))
+	conn, err := sql.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%s sslmode=disable",
+    os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PSSWRD"), os.Getenv("DB_PORT")))
 	return DB{conn}, err
 }
 
